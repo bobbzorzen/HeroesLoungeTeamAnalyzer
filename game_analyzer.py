012e62c327851b12b2a3a9953bc76c4a537854c1
Leopold Olsson <leopold.olsson@cybercom.com>
2019-02-13 12:14:42 +0100

    now analyzes all games and summarizes them in a json blob

--- game_analyzer.py
+++ game_analyzer.py
@@ -4,7 +4,27 @@ from game_data_gatherer import GameDataGatherer
 class GameAnalyzer:
 
     def __add__(self, other):
-        pass
+        if not isinstance(other, GameAnalyzer):
+            raise Exception("Invalid operation '__add__' on type " + str(type(self)) + " and " + str(type(other)))
+
+        for team in other.analysis:
+            if team not in self.analysis:
+                self.analysis[team] = {
+                    "picks": other.analysis[team]["picks"],
+                    "bans": other.analysis[team]["bans"]
+                }
+                continue
+            for pick in other.analysis[team]["picks"]:
+                if pick not in self.analysis[team]["picks"]:
+                    self.analysis[team]["picks"][pick] = {"occurrances": 0, "wins": 0, "winrate": 0}
+                self.analysis[team]["picks"][pick]["wins"] += other.analysis[team]["picks"][pick]["wins"]
+                self.analysis[team]["picks"][pick]["occurrances"] += 1
+                self.analysis[team]["picks"][pick]["winrate"] = int((self.analysis[team]["picks"][pick]["wins"] / self.analysis[team]["picks"][pick]["occurrances"])*100)
+            for ban in other.analysis[team]["bans"]:
+                if ban not in self.analysis[team]["bans"]:
+                    self.analysis[team]["bans"][ban] = {"occurrances": 1}
+                self.analysis[team]["bans"][ban]["occurrances"] += 1
+        return self
 
     def __init__(self, gameUrl):
         print("Analyzing game: ", gameUrl)
@@ -17,13 +37,15 @@ class GameAnalyzer:
                 "picks": self.getPickSummary(gameData[team]),
                 "bans": self.getBanSummary(gameData[team])
             }
-            print("TEAM: ", team)
-            print("Analysis: ", self.analysis[team])
+            # print("TEAM: ", team)
+            # print("Analysis: ", self.analysis[team])
 
     def getPickSummary(self, games):
         summary = {}
         for game in games:
             for pick in game["picks"]:
+                if(pick == ""):
+                    continue
                 if pick not in summary:
                     summary[pick] = {"occurrances": 0, "wins": 0, "winrate": 0}
                 if game["win"]:
@@ -36,6 +58,8 @@ class GameAnalyzer:
         summary = {}
         for game in games:
             for ban in game["bans"]:
+                if(ban == ""):
+                    continue
                 if ban not in summary:
                     summary[ban] = {"occurrances": 1}
                 summary[ban]["occurrances"] += 1
--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+import csv
+import json
 import requests
 from bs4 import BeautifulSoup
 from game_analyzer import GameAnalyzer
 
-teamUrl = "https://heroeslounge.gg/team/view/RIDM"
+teamUrl = "https://heroeslounge.gg/team/view/B2C"
+teamName = "Body Block Crew"
 r = requests.get(teamUrl)
 html = r.text
 parsedHTML = BeautifulSoup(html)
 matches = parsedHTML.select("#activeSeasonMatches .tab-pane .card")
+finalAnalysis = None
 for matchRow in matches:
     isScheduledMatch = len(matchRow.select(".badge-warning")) > 0
     print("is scheduled match: ", isScheduledMatch)
     if not isScheduledMatch:
         matchLink = matchRow.select(".col-2 a")[0]
         gameAnalyzer = GameAnalyzer(matchLink["href"])
-    # print("LINK: ", game)
+        if(finalAnalysis is not None):
+            finalAnalysis = finalAnalysis + gameAnalyzer
+        else:
+            finalAnalysis = gameAnalyzer
+teamData = finalAnalysis.analysis[teamName]
+
+with open('output/' + teamName + '_teamData.json', 'w') as outfile:
+    json.dump(teamData, outfile)
