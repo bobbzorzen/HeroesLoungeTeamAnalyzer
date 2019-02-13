dac6e27a1b016edcb83a5ac7e2ac9d7565db11e1
Leopold Olsson <leopold.olsson@cybercom.com>
2019-02-13 10:15:48 +0100

    added basic analysis on games

--- game_analyzer.py
+++ game_analyzer.py
@@ -3,11 +3,40 @@ from game_data_gatherer import GameDataGatherer
 
 class GameAnalyzer:
 
+    def __add__(self, other):
+        pass
+
     def __init__(self):
         gameDataGatherer = GameDataGatherer("https://heroeslounge.gg/match/view/4368")
 
         gameData = gameDataGatherer.gatherData()
-
+        analysis = {}
         for team in gameData:
+            analysis[team] = {
+                "picks": self.getPickSummary(gameData[team]),
+                "bans": self.getBanSummary(gameData[team])
+            }
             print("TEAM: ", team)
-        # print("Gamedata: ", gameData)
+            print("Analysis: ", analysis[team])
+
+    def getPickSummary(self, games):
+        summary = {}
+        for game in games:
+            for pick in game["picks"]:
+                if pick not in summary:
+                    summary[pick] = {"occurrances": 0, "wins": 0}
+                if game["win"]:
+                    summary[pick]["wins"] += 1
+                summary[pick]["occurrances"] += 1
+        return summary
+
+    def getBanSummary(self, games):
+        summary = {}
+        for game in games:
+            for ban in game["bans"]:
+                if ban not in summary:
+                    summary[ban] = {"occurrances": 1}
+                summary[ban]["occurrances"] += 1
+        return summary
+
+gameAnalyzer = GameAnalyzer()
